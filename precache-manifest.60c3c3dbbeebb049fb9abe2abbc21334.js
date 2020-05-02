b6b35f7991a7f8e4733823e071e019daba466500
Howard Yeend <howard.yeend@gmail.com>
2020-05-02 11:28:12 +0100

    Updates

---
+++ precache-manifest.60c3c3dbbeebb049fb9abe2abbc21334.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5687c0bd845dca0f5afb23b513e81fbf",
+    "url": "/gymspark/index.html"
+  },
+  {
+    "revision": "60439a2841eb10947246",
+    "url": "/gymspark/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "f869450b7b852320ae04",
+    "url": "/gymspark/static/js/2.a3543105.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gymspark/static/js/2.a3543105.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60439a2841eb10947246",
+    "url": "/gymspark/static/js/main.1177b245.chunk.js"
+  },
+  {
+    "revision": "adc40d24ae30c3f7dc31",
+    "url": "/gymspark/static/js/runtime-main.1de35f08.js"
+  }
+]);
